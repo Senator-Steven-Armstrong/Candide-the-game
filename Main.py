0a577adceaa101e9ee3cd13e0635abc7a1e9d581
Neo <neo.malmros@skola.taby.se>
2023-01-27 09:48:03 +0100

    logbook1

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ import Item_module as I
 import random as rand
 import copy
 import pygame
+import Logbook as Log
 
 pygame.mixer.init()
 
@@ -221,7 +222,7 @@ def travel():
                         
                         shop_item_4 = I.create_item(rand.choice(I.healing_item_list))
                         available_shop_items.append(shop_item_4)
-
+                        Log.shop_encounters = True
                         while True:
                             sleep(1)
                             print(f'''
@@ -388,8 +389,8 @@ Choose an action:
 3. Accessory
 
 4. Use items
-
-5. Exit inventory
+5. Open the Logbook
+6. Exit inventory
 ''')    
             while has_equipped_item == False:
 
@@ -707,9 +708,14 @@ Choose an action:
                         print_slow("\nNo consumables in inventory.\n", TEST)
 
                 elif item_change_choice == "5":
+                    #Logbook-funktion
+                    Log.logbook_menu()
+                    break
+
+                elif item_change_choice == "6":
                     break
                 else:
-                    print("\n[Please enter correct input; 1. Armor, 2. Weapon, 3. Accessory, 4. Item, 5. Exit]\n")    
+                    print("\n[Please enter correct input; 1. Armor, 2. Weapon, 3. Accessory, 4. Item, 5. Logbook, 6. Exit]\n")    
                     
                  
                 
@@ -754,6 +760,7 @@ def fight(chosen_description):
     chosen_enemy.spd = int(chosen_enemy.spd)
     chosen_enemy.exp_dropped = int(chosen_enemy.exp_dropped)
     chosen_enemy.gold_dropped = int(chosen_enemy.gold_dropped)
+    
 
     
     chosen_enemy.hp = chosen_enemy.max_hp
@@ -854,6 +861,8 @@ SPD: {chosen_enemy.spd}
 
         player.exp += chosen_enemy.exp_dropped
         print_slow(f"\nYou gained {chosen_enemy.exp_dropped} exp!\n", TEST)
+
+        chosen_enemy.encounters = True
 
         loot("enemy drop")
 
--- logbook.py
+++ logbook.py
@@ -1,3 +1,12 @@
+from time import sleep
+import sys,time
+import Enemy_module as E
+import random as rand
+
+TEST = 0.02
+PUNCTUATION_PAUSE_TIME = 0.4
+COMMA_PAUSE_TIME = 0.15
+SEMICOLON_PAUSE_TIME = 0.6
 # This is a logbook of lore entries on the different enemies. 
 # Here, you can learn about the enemies backstory and appearance, and also some
 # common attributes that they may possess, such as increased speed or higher health.
@@ -5,46 +14,70 @@
 
 #Foes Category
 
-bandit_lore = """
+def print_slow(str, write_speed):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(write_speed)
+        if letter == "." or letter == "?" or letter == "!":
+            sleep(PUNCTUATION_PAUSE_TIME)
+        elif letter == ",":
+            sleep(COMMA_PAUSE_TIME)
+        elif letter == ";":
+            sleep(SEMICOLON_PAUSE_TIME)
+
+bandit_lore = '''
 Bandits are one of the most common foes you will meet. They roam the land, looking for 
 innocents to steal from and kill. Bandits are quite average in every category of a fight, 
 but that does not mean they should be underestimated.
-"""
+'''
 
-cannibal_lore = """
+cannibal_lore = '''
 Cannibals are the subordinates of the Långöron. They are generally weaker than them,
 but there are stronger individuals among them that can rival their superiors in strength.
 They are ferocious and when they've got your scent they will not cease their hunt until stopped.
 Some cannibals have left their original tribe to start their own in the jungle, so you may encounter them often.
-"""
+'''
 
-långöron_lore = """
+långöron_lore = '''
 The Långöron are the leaders of the cannibal tribes are at the physical peak. They often stand over 7 feet tall,
 and carry weapons that can deal massive damage, and their training has made them very fast. It is believed they have a certain connection to nature, 
 and follow a deity by the name of Eivor. This may be the cause of their physical prowess.
-"""
+'''
 
-traveler_lore = """
+traveler_lore = '''
 Travelers are very large humans, towering above the average person. They carry an enormous sword in honor 
 of their culture. This sword, combined with superior size, provides a very large advantage in combat. 
 While Travelers are very strong, due to their size they fall behind in speed. 
 Legends say Travelers also have capabilities of magic. Do not challenge these foes if you are not ready.
-"""
+'''
 
-goblin_lore = """
+goblin_lore = '''
 Goblins are lowly creatures, often found in packs looking for people to steal from and kill for sport.
 They are very weak except for being extremely fast. Despite being weak alone, their strength is in their numbers.
 Do not approach if your speed is low, and do not linger in the heat of battle, for others could be on their way.
-"""
+'''
+
+baronen_lore = '''
+Baronen, short for Baron Thunder-ten-tronckh, is the head of a castle that goes by the same name. 
+His strength is immaculate, and the mere thought of challenging him is dangerous. 
+His name shall be remembered for long, and it's power will have an effect on the world long after he is gone.
+'''
+
+pococurante_lore = '''
+Pococurante is one of the most powerful men in Venice. He has an exquisite taste for wine, 
+but not nearly such respect for women. He is a very petty man, if any person has an opinion not equal to his,
+you shall not be allowed to live further. He is the physical embodiment of danger.
+'''
 
 #Friends Category
 
-kunigunda_lore = """
+kunigunda_lore = '''
 The sweet Kunigunda is Candide's betrothen. Candide is currently looking for her,
 in hope that they might one day be together again.
-"""
+'''
 
-shopkeeper_lore = """
+shopkeeper_lore = '''
 Not much is known about the shopkeeper. He always manages to find us on our journey
 despite our constant traveling to many different locations. This is especially curious,
 since we have never told him where we are going. 
@@ -52,4 +85,218 @@ since we have never told him where we are going.
 It seems like what happens never surprises him, almost as if he knows what will happen.
 We have asked him several times how he manages to obtain these items, but he always
 laughs it off saying 'I have my ways'.
-"""
+'''
+
+shop_encounters = False
+#entries_list=[E.bandit_encounters, E.cannibal_encounters, E.långöron_encounters, E.traveler_encounters, E.goblin_encounters, E.kunigunda_encounters, E.baronen_encounters, E.pococurante_encounters, shop_encounters]
+
+# Function to use the logbook menu
+def logbook_menu():
+    while True:
+        print_slow('''
+        Welcome to the logbook! Surely you have met others on this long journey of yours, 
+        whether they are friends or foes. Once you have met (often killed) these fellows, 
+        you will unlock a logbook entry here giving information about them. Good luck!
+        ''')
+
+        sleep(3)
+
+        print_slow('''
+        These are the entrys you have unlocked:
+        ''')
+
+        if E.bandit.encounters == True:
+            print('''
+            1. Bandits
+            ''')
+
+        if E.cannibal.encounters == True:
+            print('''
+            2. Cannibals
+            ''')
+        
+        if E.långöron_encounters == True:
+            print('''
+            3. The Långöron
+            ''')
+
+        if E.traveler_encounters == True:
+            print('''
+            4. Travelers
+            ''')
+
+        if E.goblin_encounters == True:
+            print('''
+            5. Goblins
+            ''')
+
+        if E.kunigunda_encounters == True:
+            print('''
+            6. Kunigunda
+            ''')
+
+        if E.baronen_encounters == True:
+            print('''
+            7. Baronen
+            ''')
+        
+        if E.pococurante_encounters == True:
+            print('''
+            8. Pococurante
+            ''')
+
+        if shop_encounters == True:
+            print('''
+            9. The Shopkeeper
+            ''')
+
+        int(input("Select one of the options."))
+        if input == "1":
+            while True:
+        
+                print_slow(bandit_lore)
+                sleep(3)
+                input("Would you like to go back? Y/N")
+                if input == "Y" or input == "y":
+                    break
+                elif input == "N" or input == "n":
+                    input("Write Y when you are ready to leave.")
+                    if input == "Y" or input == "y":
+                        break
+                    else:
+                        print_slow("Try again.")
+                        continue
+
+                input("Write Y when you are ready to leave.")
+        if input == "2":
+            while True:
+            
+                print_slow(cannibal_lore)
+                sleep(3)
+                input("Would you like to go back? Y/N")
+                if input == "Y" or input == "y":
+                    break
+                elif input == "N" or input == "n":
+                    input("Write Y when you are ready to leave.")
+                    if input == "Y" or input == "y":
+                        break
+                    else:
+                        print_slow("Try again.")
+                        continue
+
+                input("Write Y when you are ready to leave.")
+        if input == "3":
+            while True:
+            
+                print_slow(långöron_lore)
+                sleep(3)
+                input("Would you like to go back? Y/N")
+                if input == "Y" or input == "y":
+                    break
+                elif input == "N" or input == "n":
+                    input("Write Y when you are ready to leave.")
+                    if input == "Y" or input == "y":
+                        break
+                    else:
+                        print_slow("Try again.")
+                        continue
+
+                input("Write Y when you are ready to leave.")
+        if input == "4":
+            while True:
+            
+                print_slow(traveler_lore)
+                sleep(3)
+                input("Would you like to go back? Y/N")
+                if input == "Y" or input == "y":
+                    break
+                elif input == "N" or input == "n":
+                    input("Write Y when you are ready to leave.")
+                    if input == "Y" or input == "y":
+                        break
+                    else:
+                        print_slow("Try again.")
+                        continue
+
+                input("Write Y when you are ready to leave.")
+        if input == "5":
+            while True:
+
+                print_slow(goblin_lore)
+                sleep(3)
+                input("Would you like to go back? Y/N")
+                if input == "Y" or input == "y":
+                    break
+                elif input == "N" or input == "n":
+                    input("Write Y when you are ready to leave.")
+                    if input == "Y" or input == "y":
+                        break
+                    else:
+                        print_slow("Try again.")
+                        continue
+
+                input("Write Y when you are ready to leave.")
+
+            if input == "6":
+                while True:
+                
+                    print_slow(kunigunda_lore)
+                    sleep(3)
+                    input("Would you like to go back? Y/N")
+                    if input == "Y" or input == "y":
+                        break
+                    elif input == "N" or input == "n":
+                        input("Write Y when you are ready to leave.")
+                        if input == "Y" or input == "y":
+                            break
+                        else:
+                            print_slow("Try again.")
+                            continue
+
+            if input == "7":
+                while True:
+                
+                    print_slow(baronen_lore)
+                    sleep(3)
+                    input("Would you like to go back? Y/N")
+                    if input == "Y" or input == "y":
+                        break
+                    elif input == "N" or input == "n":
+                        input("Write Y when you are ready to leave.")
+                        if input == "Y" or input == "y":
+                            break
+                        else:
+                            print_slow("Try again.")
+                            continue
+
+            if input == "8":
+                while True:
+                
+                    print_slow(pococurante_lore)
+                    sleep(3)
+                    input("Would you like to go back? Y/N")
+                    if input == "Y" or input == "y":
+                        break
+                    elif input == "N" or input == "n":
+                        input("Write Y when you are ready to leave.")
+                        if input == "Y" or input == "y":
+                            break
+                        else:
+                            print_slow("Try again.")
+                            continue
+
+            if input == "9":
+                while True:
+                
+                    print_slow(shopkeeper_lore)
+                    sleep(3)
+                    input("Would you like to go back? Y/N")
+                    if input == "Y" or input == "y":
+                        break
+                    elif input == "N" or input == "n":
+                        input("Write Y when you are ready to leave.")
+                        if input == "Y" or input == "y":
+                            break
+                        else:
+                            print_slow("Try again.")
+                            continue
